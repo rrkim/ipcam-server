d48a40a8ce8b53e4dd11f4d88f354083153f0e6b
Ryangrae Kim <dev@rrkim.com>
2024-04-06 13:28:25 +0900

    최초 커밋

---
+++ src/main/java/com/rrkim/ipcamserver/IpcamServerApplication.java
@@ -0,0 +1,13 @@
+package com.rrkim.ipcamserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IpcamServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IpcamServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rrkim/ipcamserver/IpcamServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rrkim.ipcamserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IpcamServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
