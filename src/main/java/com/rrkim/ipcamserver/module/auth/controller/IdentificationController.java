b9c7f10ba602412b67b8c46ab3f8492d12cc4e96
gmelan <gmelanals@gmail.com>
2024-05-12 13:42:38 +0900

    feat: tci 파일 다운로드 엔드포인트에 대응하는 IdentificationController 추가

---
+++ src/main/java/com/rrkim/ipcamserver/module/auth/controller/IdentificationController.java
@@ -0,0 +1,40 @@
+package com.rrkim.ipcamserver.module.auth.controller;
+
+import com.rrkim.ipcamserver.core.file.service.FileService;
+import com.rrkim.ipcamserver.module.auth.service.IdentificationService;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.io.DataInputStream;
+import java.io.IOException;
+
+@Controller
+@RequiredArgsConstructor
+public class IdentificationController {
+    private final IdentificationService identificationService;
+    private final FileService fileService;
+
+    @GetMapping("getpair")
+    public void getPair(HttpServletResponse response) throws IOException {
+        // make sure camera identity is present
+        // create camera identity and generate key pair(.tci)
+        // TODO: get key pair for multiple clients
+        identificationService.createCameraIdentity();
+
+        DataInputStream bos = null;
+        try {
+            bos = fileService.getFileOutputStream("keypair.tci");
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        response.setHeader("Content-Type", "text/plain");
+        if (bos == null) {
+            return;
+        }
+        StreamUtils.copy(bos, response.getOutputStream());
+    }
+}
