75b9f784407da90515f0c0aae9840b25102cd9e3
gmelan <gmelanals@gmail.com>
2024-05-13 11:23:37 +0900

    test: 클라이언트 공유 키 수신 시나리오 추가

--- src/test/java/com/rrkim/ipcamserver/IpcamServerApplicationTests.java
+++ src/test/java/com/rrkim/ipcamserver/IpcamServerApplicationTests.java
@@ -86,4 +86,56 @@ public class IpcamServerApplicationTests {
             assert decrypted[i] == targetString.getBytes()[i];
         }
     }
+
+    @Test
+    void RSAAndAESTest() throws Exception {
+        /*
+         * when client received shared key encrypted with public key
+         * - decrypt shared key with secret key
+         * - encrypt sample data with shared key
+         * - decrypt sample data with shared key
+         */
+
+        // make sure camera identity(RSA key pair) is present
+        identificationService.createCameraIdentity();
+
+        // target text
+        final String targetString = "hello world";
+
+        // keyFactory
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        Cipher cipherRSA = Cipher.getInstance("RSA");
+        Cipher cipherAES = Cipher.getInstance("AES");
+
+        // get RSA-encrypted AES-key
+        byte[] encryptedSharedKey = identificationService.getEncryptedSharedKey();
+
+        // 1. decrypt AES-key with secret key
+        String privateKeyString = fileService.readFileByDataStream("keys/private.key");
+        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
+        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        Key privateKey = keyFactory.generatePrivate(privateKeySpec);
+
+        cipherRSA.init(Cipher.DECRYPT_MODE, privateKey);
+
+        byte[] sharedKey = cipherRSA.doFinal(encryptedSharedKey);
+
+        // 2. encrypt target string with AES-key
+        SecretKeySpec secretKeySpec = new SecretKeySpec(sharedKey, "AES");
+        cipherAES.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+
+        byte[] encryptedTargetString = cipherAES.doFinal(targetString.getBytes());
+
+        // 3. decrypt target string with AES-key
+        cipherAES.init(Cipher.DECRYPT_MODE, secretKeySpec);
+        byte[] decryptedTargetString = cipherAES.doFinal(encryptedTargetString);
+
+        // 4. test result
+        assert decryptedTargetString.length == targetString.getBytes().length;
+
+        for(int i = 0; i < decryptedTargetString.length; i++) {
+            assert decryptedTargetString[i] == targetString.getBytes()[i];
+        }
+
+    }
 }
