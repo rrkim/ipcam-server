8c4a31b68372ef6dbe6cc6f7ebe4404062a8482b
gmelan <gmelanals@gmail.com>
2024-05-20 16:20:17 +0900

    test: RSA, AES, 대칭 키 교환 테스트 코드 추가

--- src/test/java/com/rrkim/ipcamserver/IpcamServerApplicationTests.java
+++ src/test/java/com/rrkim/ipcamserver/IpcamServerApplicationTests.java
@@ -1,8 +1,11 @@
 package com.rrkim.ipcamserver;
 
-import com.rrkim.ipcamserver.core.file.service.FileService;
+import com.rrkim.ipcamserver.core.configuration.service.DeviceConfigService;
+import com.rrkim.ipcamserver.core.utility.AesUtility;
 import com.rrkim.ipcamserver.core.utility.FileUtility;
-import com.rrkim.ipcamserver.core.utility.StringUtility;
+import com.rrkim.ipcamserver.core.utility.JsonUtility;
+import com.rrkim.ipcamserver.core.utility.RsaUtility;
+import com.rrkim.ipcamserver.module.auth.dto.RSAKeyPair;
 import com.rrkim.ipcamserver.module.auth.dto.SecureKey;
 import com.rrkim.ipcamserver.module.auth.service.IdentificationService;
 import org.junit.jupiter.api.Test;
@@ -13,6 +16,7 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.security.KeyFactory;
+import java.security.PrivateKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
@@ -22,81 +26,26 @@ public class IpcamServerApplicationTests {
     @Autowired
     IdentificationService identificationService;
 
+    final String TARGET_STRING = "Hello World!";
+
     @Test
     void AESTest() throws Exception {
-        // target text
-        String targetString = "hello world";
+        String key = AesUtility.getRandomAesKey();
 
-        Cipher cipher = Cipher.getInstance("AES");
+        String encrypted = AesUtility.encodeAesCbc(TARGET_STRING.getBytes(), key);
+        String decrypted = new String(AesUtility.decodeAesCbc(encrypted, key));
 
-        // encrypt target text with shared key
-        identificationService.createSymmetricKey();
-        byte[] key = identificationService.getSymmetricKey().getBytes();
-
-        // ensure key is equal to shared key file
-        byte[] sharedKeyFromFile = Base64.getDecoder().decode(FileUtility.readFileByDataStream("keys/shared.key"));
-
-        assert key.length == sharedKeyFromFile.length;
-        for(int i = 0; i < sharedKeyFromFile.length; i++) {
-            assert sharedKeyFromFile[i] == key[i];
-        }
-
-
-        SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-
-        byte[] encrypted = cipher.doFinal(targetString.getBytes());
-
-        // decrypt target text with same key
-        cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-        byte[] decrypted = cipher.doFinal(encrypted);
-
-        // test result
-        assert decrypted.length == targetString.getBytes().length;
-
-        for(int i = 0; i < decrypted.length; i++) {
-            assert decrypted[i] == targetString.getBytes()[i];
-        }
+        assert TARGET_STRING.equals(decrypted);
     }
 
     @Test
     void RSATest() throws Exception {
-        // make sure camera identity(RSA key pair) is present
-        identificationService.createCameraIdentity();
+        RSAKeyPair keyPair = RsaUtility.createKeyPair();
 
-        // target text
-        String targetString = "hello world";
+        String encrypted = RsaUtility.encryptData(TARGET_STRING, keyPair.getPublicKey());
+        String decrypted = RsaUtility.decryptData(encrypted, keyPair.getPrivateKey());
 
-        // keyFactory
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        Cipher cipher = Cipher.getInstance("RSA");
-
-        // encrypt target text with public key
-        String publicKeyString = FileUtility.readFileByDataStream("keys/public.key");
-        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyString);
-        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
-        Key publicKey = keyFactory.generatePublic(keySpec);
-
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encrypted = cipher.doFinal(targetString.getBytes());
-
-        // decrypt encrypted sequence with private key
-        String privateKeyString = FileUtility.readFileByDataStream("keys/private.key");
-        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
-        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-        Key privateKey = keyFactory.generatePrivate(privateKeySpec);
-
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-
-        byte[] decrypted = cipher.doFinal(encrypted);
-
-        // verify original string and decrypted string are same
-
-        assert decrypted.length == targetString.getBytes().length;
-
-        for(int i = 0; i < decrypted.length; i++) {
-            assert decrypted[i] == targetString.getBytes()[i];
-        }
+        assert decrypted.equals(TARGET_STRING);
     }
 
     @Test
@@ -111,81 +60,24 @@ public class IpcamServerApplicationTests {
         // make sure camera identity(RSA key pair) is present
         identificationService.createCameraIdentity();
 
-        // target text
-        final String targetString = "hello world";
+        // generate RSA-encrypted secure key
+        identificationService.createSymmetricKey();
 
-        // keyFactory
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        Cipher cipherRSA = Cipher.getInstance("RSA");
-        Cipher cipherAES = Cipher.getInstance("AES");
+        // SERVER: server-owned symmetric key
+        String serverSymmetricKey = identificationService.getSymmetricKey();
 
-        // get RSA-encrypted AES-key
-        SecureKey secureKey = identificationService.getSecretKey();
-        String secureKeyString = secureKey.getSecureKey();
-        byte[] encryptedSharedKey = StringUtility.decodeBase64Bytes(secureKeyString);
+        // CLIENT: read TCI file and get private key
+        String tciString = FileUtility.readFileByDataStream("keys/3859b5ab-e9e5-4630-a61c-e1814c323e1a.tci");
+        String tciConfiguration = new String(Base64.getDecoder().decode(tciString));
+        String privateKeyString = JsonUtility.getProperty(tciConfiguration, "credential");
 
-        // 1. decrypt AES-key with secret key
-        String privateKeyString = FileUtility.readFileByDataStream("keys/private.key");
-        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
-        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-        Key privateKey = keyFactory.generatePrivate(privateKeySpec);
+        assert privateKeyString != null;
 
-        cipherRSA.init(Cipher.DECRYPT_MODE, privateKey);
+        // CLIENT: get secure key and decrypt with private key
+        SecureKey secureKey = identificationService.getSecureKey();
 
-        byte[] sharedKey = cipherRSA.doFinal(encryptedSharedKey);
+        String clientSymmtricKey = RsaUtility.decryptData(secureKey.getSecureKey(), privateKeyString);
 
-        // verify decrypted key equals shared key file
-        byte[] sharedKeyFromFile = Base64.getDecoder().decode(FileUtility.readFileByDataStream("keys/shared.key"));
-
-        assert sharedKey.length == sharedKeyFromFile.length;
-
-        for(int i = 0; i < sharedKey.length; i++) {
-            assert sharedKey[i] == sharedKeyFromFile[i];
-        }
-
-        // 2. encrypt target string with AES-key
-        SecretKeySpec secretKeySpec = new SecretKeySpec(sharedKey, "AES");
-        cipherAES.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-
-        byte[] encryptedTargetString = cipherAES.doFinal(targetString.getBytes());
-
-        // 3. decrypt target string with AES-key
-        cipherAES.init(Cipher.DECRYPT_MODE, secretKeySpec);
-        byte[] decryptedTargetString = cipherAES.doFinal(encryptedTargetString);
-
-        // 4. test result
-        assert decryptedTargetString.length == targetString.getBytes().length;
-
-        for(int i = 0; i < decryptedTargetString.length; i++) {
-            assert decryptedTargetString[i] == targetString.getBytes()[i];
-        }
-
-    }
-
-    @Test
-    void PrivateKeyDecryptTest() throws Exception {
-        // RSA encrypted shared key string from http://localhost:8080/sharedKey
-        final String SHARED_KEY = "hWx4WgQ++57oY+Ji+d48aZQTzqyD0f2Fg7Cml94WC374SsGl6b23iuZWvkcRq9hHLB2s6j7WEeVDf3uwhMab+7WsGiwmRsThrcBYmNzIKMU8KfJx31X6HyPRXLl95b5FiycrUsNLOBnJFj79tDKwyMSxjOAH0fSnInxpzyMnXw87O1pw96ti03jtZKkccCLGnW90N4sBOypWCtzVFyDMszS507G5LDj6GMt8p3hUgZzEcxJHjYt8XPaFs4zMWFuvHtHcedDaCUx6VPVeg1QUgLVnBwMIxg/7KvVvbfW+KDhB81OSxOzAVQd9RWG3P7JwuFChqeHltNIPM3nM57X3Dw==";
-
-        // 1. proceed RSA decrypt with private-key
-        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-        Cipher cipherRSA = Cipher.getInstance("RSA");
-
-        String privateKeyString = FileUtility.readFileByDataStream("keys/private.key");
-        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyString);
-        PKCS8EncodedKeySpec privateKeySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
-        Key privateKey = keyFactory.generatePrivate(privateKeySpec);
-
-        cipherRSA.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] sharedKey = cipherRSA.doFinal(Base64.getDecoder().decode(SHARED_KEY.getBytes()));
-
-        // 2. verify decrypted shared-key is equal shared-key from file
-        byte[] sharedKeyFromFile = Base64.getDecoder().decode(FileUtility.readFileByDataStream("keys/shared.key"));
-
-        assert sharedKey.length == sharedKeyFromFile.length;
-        for(int i = 0; i < sharedKeyFromFile.length; i++) {
-            assert sharedKeyFromFile[i] == sharedKey[i];
-        }
-
+        assert serverSymmetricKey.equals(clientSymmtricKey);
     }
 }
